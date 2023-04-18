69fe9ae1f5a6ac1b6a5cf9370bf8c416d6e4b13b
dtech <github@dtech.club>
2023-04-18 10:00:41 +0200

    movieModel, movieSchema, movieRoutes

---
+++ controllers/movieController.js
@@ -0,0 +1,45 @@
+const Movie = require('../models/movieSchema');
+
+// Controller functions for movie routes
+exports.getAllMovies = async (req, res) => {
+    try {
+        const movies = await Movie.find();
+        res.json(movies);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+exports.getMovieById = async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+        if (!movie) {
+            return res.status(404).json({ message: 'Movie not found' });
+        }
+        res.json(movie);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+exports.createMovie = async (req, res) => {
+    try {
+        const movie = new Movie(req.body);
+        const savedMovie = await movie.save();
+        res.json(savedMovie);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+exports.updateMovie = async (req, res) => {
+    try {
+        const movie = await Movie.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!movie) {
+            return res.status(404).json({ message: 'Movie not found' });
+        }
+        res.json(movie);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
