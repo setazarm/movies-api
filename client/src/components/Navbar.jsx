c68c95bb6586730e6db83df4160f40c64dd95dbe
Setare <zahrasadat.azarmsa@dci.education>
2023-04-19 18:55:44 +0200

    add documentation component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,8 +7,9 @@ export default function Navbar() {
   return (
     <nav>
       <ul>
-       
+      <li><NavLink to="/">Home </NavLink></li>
         <li><NavLink to="/register">Register </NavLink></li>
+        <li><NavLink to="/documentation">Documentation </NavLink></li>
       </ul>
     </nav>
   )
