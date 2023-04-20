14cde581e807747ee94164183ffbd826355617d4
Setare <zahrasadat.azarmsa@dci.education>
2023-04-20 15:35:11 +0200

    change addMovie toUser logic

--- server/middleware/validator.js
+++ server/middleware/validator.js
@@ -1,15 +1,15 @@
-import {body, validationResult} from "express-validator"
+import { body, validationResult } from "express-validator"
 
-export const rules= [body("email").isEmail().withMessage("please provide us with valid email").
-normalizeEmail(),
+export const rules = [body("email").isEmail().withMessage("please provide us with valid email").
+    normalizeEmail(),
 body("password").isString().withMessage("password should be string")
-.isLength({min:4}).withMessage("password is too short") ,
+    .isLength({ min: 4 }).withMessage("password is too short"),
 
-(req,res,next)=>{
-const errors= validationResult(req)
-if(errors.isEmpty()){
-    next()
-}else{
-    res.json({success:false, message:errors.array().map(err=>({[err.param]:err.msg}))})
-}
+(req, res, next) => {
+    const errors = validationResult(req)
+    if (errors.isEmpty()) {
+        next()
+    } else {
+        res.json({ success: false, message: errors.array().map(err => ({myError: err.msg })) })
+    }
 }]
